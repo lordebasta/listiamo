2787d2de9a7e88cd6700f1e2c2478d225e517a7d
lordebasta <41568059+lordebasta@users.noreply.github.com>
2024-04-30 11:44:26 +0200

    improved ui

--- lists/urls.py
+++ lists/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path("<uuid:list_id>/del", views.delete_item, name="delete_item"),
     path("<uuid:list_id>/create", views.create_item, name="create_item"),
     path("create", views.create_list, name="create_list")
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
