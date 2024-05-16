cef12e7f937dd893afb568406bfee4f2d7a9242c
lordebasta <41568059+lordebasta@users.noreply.github.com>
2024-05-16 15:34:02 +0200

    add tests for list_repo

--- lists/list_repo.py
+++ lists/list_repo.py
@@ -9,10 +9,7 @@ def get_lists() -> QuerySet:
 
 
 def get_list(list_id: uuid.uuid4) -> ListModel:
-    try:
-        list = ListModel.objects.get(pk=list_id)
-    except ListModel.DoesNotExist as e:
-        raise e
+    list = ListModel.objects.get(pk=list_id)
     list.last_visited = date.today()
     list.save()
     return list
@@ -26,19 +23,21 @@ def create_list(list_name: str) -> ListModel:
     return list
 
 
-def delete_item(list_id: uuid.uuid4, item_id: int) -> None:
-    try:
-        ListModel.objects.get(pk=list_id).item_set.get(pk=item_id).delete()
-    except (ListModel.DoesNotExist, Item.DoesNotExist) as e:
-        raise e
+def create_item(list_id: uuid.uuid4, item_name: str, item_link: str = "") -> None:
+    if item_name == "":
+        raise ValueError("item_name can't be empty")
+    if len(item_name) > 100:
+        raise ValueError("The list name can be at max 100 characters long.")
+    if len(item_link) > 255:
+        raise ValueError("The list link can be at max 255 characters long.")
 
-
-def create_item(list_id: uuid.uuid4, item_name: str, item_link: str) -> None:
-    try:
-        list = get_list(list_id)
-    except ListModel.DoesNotExist as e:
-        raise e
+    list = get_list(list_id)
     if item_link and not item_link.startswith("https://") and not item_link.startswith("http://"):
         item_link = "//" + item_link
     item = Item(list=list, name=item_name, link=item_link)
     item.save()
+
+
+def delete_item(list_id: uuid.uuid4, item_id: int) -> None:
+    list_obj = get_list(list_id)
+    list_obj.item_set.get(pk=item_id).delete()
