5dd9463e4a84a2a49220f2b504fc9c7a71bf9c07
lordebasta <41568059+lordebasta@users.noreply.github.com>
2024-05-24 10:54:34 +0200

    add favicon

--- lists/urls.py
+++ lists/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "lists"
 urlpatterns = [
+    path("favicon.ico", views.favicon),
     path("", views.get_lists, name="index"),
     path("<uuid:pk>/", views.get_list, name="detail"),
     path("<uuid:list_id>/del", views.delete_item, name="delete_item"),
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,18 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.http import Http404
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views import generic
-from datetime import date
+from django.conf import settings
+from django.http import FileResponse, Http404, HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import require_GET
 
 from . import list_repo
 from .models import ListModel, Item
+
+
+@require_GET
+@cache_control(max_age=60 * 60 * 24, immutable=True, public=True)  # one day
+def favicon(request: HttpRequest) -> HttpResponse:
+    file = (settings.BASE_DIR / "static" / "favicon.png").open("rb")
+    return FileResponse(file)
 
 
 def get_lists(request):
