a9e90a48a2f50c325af2abfce4219152bdb54d32
lordebasta <41568059+lordebasta@users.noreply.github.com>
2024-06-05 14:51:51 +0200

    removed auth

--- listiamo/settings.py
+++ listiamo/settings.py
@@ -124,6 +124,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
